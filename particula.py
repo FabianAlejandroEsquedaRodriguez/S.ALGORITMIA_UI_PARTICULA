6cc7f5cc90b5472f77955b12ee64253905d690ea
Fabián Alejandro Esqueda Rodríguez <fabian.esqueda2892@alumnos.udg.mx>
2021-05-08 22:12:40 -0500

    Se modifico el metodo __str__, ya que falto convertir la velocidad, y asi, poder imprimirla en el PlainTextEdit

--- particula.py
+++ particula.py
@@ -21,6 +21,7 @@ class Particula:
             'ORIGEN Y: ' + str(self.__origen_y) + '\n' +
             'DESTINO X: ' + str(self.__destino_x) + '\n' +
             'DESTINO Y: ' + str(self.__destino_y) + '\n' +
+            'VELOCIDAD: ' +str(self.velocidad) + '\n' +
             'RED: ' + str(self.__red) + '\n' +
             'GREEN: ' + str(self.__green) + '\n' +
             'BLUE: ' + str(self.__blue) + '\n' +
